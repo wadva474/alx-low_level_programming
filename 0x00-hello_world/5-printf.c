bb4a077c96e934028b2a77a409520fa11d01828e
wadud <wadva474@gmail.com>
2023-03-16 19:17:04 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - print a C programme using print f
+ *
+ * Returns: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
